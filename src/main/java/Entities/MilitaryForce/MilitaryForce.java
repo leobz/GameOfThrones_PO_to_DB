00eeb5344d4ac4b3f3a70e0be798cba988a2e167
Leonel Bazan <leo.bazan.x@gmail.com>
2020-09-14 15:43:00 -0300

    JPA: Add SINGLE_TABLE inheritance strategy on MilitaryForce

--- src/main/java/Entities/MilitaryForce/MilitaryForce.java
+++ src/main/java/Entities/MilitaryForce/MilitaryForce.java
@@ -1,7 +1,13 @@
 package Entities.MilitaryForce;
 
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
 import Entities.Place.Place;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class MilitaryForce {
 	void atack_to(Place place) {
 	}
